b554bde1dca042e494396eba592a725df0d0ca32
Aaron Duce <aaronduce@lymelite.co.uk>
2022-02-03 10:13:16 +0000

    Accidentally removed colors override div to get tailwind to compile correctly.

--- app/routes/subnets/new.tsx
+++ app/routes/subnets/new.tsx
@@ -48,7 +48,7 @@ export default function NewSubnetRoute() {
                     <option value="10" selected>10</option>
                     <option value="172">172</option>
                     <option value="192">192</option>
-                </select> . <input type="number" name="v4partTwo" min="0" max="255" value="0" className="text-slate-100 bg-slate-900 border-slate-900 rounded-lg w-24" /> . <input type="number" name="v4partThree" min="0" max="255" value="0" className="text-slate-100 bg-slate-900 border-slate-900 rounded-lg w-24" /> . 000 <br />
+                </select> . <input type="number" name="v4partTwo" min="0" max="255" defaultValue="0" className="text-slate-100 bg-slate-900 border-slate-900 rounded-lg w-24" /> . <input type="number" name="v4partThree" min="0" max="255" defaultValue="0" className="text-slate-100 bg-slate-900 border-slate-900 rounded-lg w-24" /> . 000 <br />
                 <i className="text-sm text-slate-500/50">* Addressing restrictions as per <a href="https://www.ietf.org/rfc/rfc1918.txt">RFC 1918: Address Allocation for Private Internets</a></i>
               </label>
             </div>
@@ -74,6 +74,13 @@ export default function NewSubnetRoute() {
               </button>
             </div>
           </Form>
+
+          <div className="hidden ">bg-red-900 bg-orange-900 bg-amber-900 bg-yellow-900 bg-lime-900 bg-green-900 bg-emerald-900 bg-teal-900 bg-cyan-900 bg-sky-900 bg-blue-900 
+          bg-indigo-900 bg-violet-900 bg-purple-900 bg-fuchsia-900 bg-pink-900 bg-rose-900 bg-slate-900 bg-neutral-900 bg-red-900/50 bg-orange-900/50 bg-amber-900/50 bg-yellow-900/50
+          bg-lime-900/50 bg-green-900/50 bg-emerald-900/50 bg-teal-900/50 bg-cyan-900/50 bg-sky-900/50 bg-blue-900/50 bg-indigo-900/50 bg-violet-900/50 bg-purple-900/50 bg-fuchsia-900/50
+          bg-pink-900/50 bg-rose-900/50 bg-slate-900/50 bg-neutral-900/50 shadow-red-900 shadow-orange-900 shadow-amber-900 shadow-yellow-900 shadow-lime-900 shadow-green-900 shadow-emerald-900
+          shadow-teal-900 shadow-cyan-900 shadow-sky-900 shadow-blue-900 shadow-indigo-900 shadow-violet-900 shadow-purple-900 shadow-fuchsia-900 shadow-pink-900 shadow-rose-900 shadow-slate-900
+          shadow-neutral-900</div>
         </div>
     );
 }
