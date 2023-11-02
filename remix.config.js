79b54f1634e6fede9cdde2098c51a00879948062
Aaron Duce <aaron@duce.me.uk>
2023-11-02 15:22:52 +0000

    Upgrades to 1.19 remix, and other packages

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,37 @@
 /**
- * @type {import('@remix-run/dev/config').AppConfig}
+ * @type {import('@remix-run/dev').AppConfig}
  */
+
+const {
+  createRoutesFromFolders,
+} = require("@remix-run/v1-route-convention");
+
 module.exports = {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "build",
-  devServerPort: 8002,
-  ignoredRouteFiles: [".*"]
+  ignoredRouteFiles: [".*"],
+  serverDependenciesToBundle: [],
+  
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+  // devServerPort: 8002,
+  
+  future: {
+    // makes the warning go away in v1.15+
+    v2_routeConvention: true,
+    v2_headers: true,
+    v2_errorBoundary: false,
+    v2_normalizeFormMethod: true,
+    v2_dev: false,
+    v2_meta: false
+  },
+
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes);
+  },
+
+  serverModuleFormat: "cjs",
+
+  tailwind: true,
 };
